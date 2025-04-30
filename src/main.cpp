a0ef9eed36add6c7abb62072896d44b857d8f507
FotiosBistas <fotis.bistas@gmail.com>
2025-04-30 19:38:42 +0300

    Build a general structure
    Signed-off-by: FotiosBistas <fotis.bistas@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,89 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
+const char* vertex_shader_src = R"(
+#version 330 core
+layout(location = 0) in vec3 coord3d;
+void main() {
+    gl_Position = vec4(coord3d, 1.0);
+}
+)";
+
+const char* fragment_shader_src = R"(
+#version 330 core
+out vec4 out_color;
+void main() {
+    out_color = vec4(1.0, 0.5, 0.2, 1.0);
+}
+)";
+
+GLuint createShader(GLenum type, const char* src) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
+    return shader;
+}
 
 int main() {
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
-        return -1;
-    }
-
-    SDL_Window* window = SDL_CreateWindow("OpenGL Window",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        800, 600, SDL_WINDOW_OPENGL);
-
-    if (!window) {
-        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
-        SDL_Quit();
-        return -1;
-    }
-
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Triangle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
-    if (!context) {
-        std::cerr << "SDL_GL_CreateContext failed: " << SDL_GetError() << "\n";
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+    glewInit();
 
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "glewInit failed\n";
-        SDL_GL_DeleteContext(context);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+    float vertices[] = {
+        0.0f,  0.5f, 0.0f,
+       -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
 
-    glm::vec3 position(1.0f, 2.0f, 3.0f);
-    glm::vec3 direction = glm::normalize(position);
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), position);
+    GLuint vbo, vao;
+    glGenBuffers(1, &vbo);
+    glGenVertexArrays(1, &vao);
 
-    std::cout << "GLM test: normalized direction = ("
-            << direction.x << ", "
-            << direction.y << ", "
-            << direction.z << ")\n";
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
+    GLuint vs = createShader(GL_VERTEX_SHADER, vertex_shader_src);
+    GLuint fs = createShader(GL_FRAGMENT_SHADER, fragment_shader_src);
+    
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
 
     bool running = true;
     SDL_Event event;
-
     while (running) {
         while(SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
+            if(event.type == SDL_QUIT)
                 running = false;
         }
 
-        glClearColor(0.2f, 0.1f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        glUseProgram(0);
+
         SDL_GL_SwapWindow(window);
     }
 
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
     SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
-
