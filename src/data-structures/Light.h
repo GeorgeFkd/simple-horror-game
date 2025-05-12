61e2441b7c50611a20051844c4fb406fa4dcf315
FotiosBistas <fotis.bistas@gmail.com>
2025-05-12 11:17:08 +0300

    Initialize depth buffers for each light
    Signed-off-by: FotiosBistas <fotis.bistas@gmail.com>

--- src/data-structures/Light.h
+++ src/data-structures/Light.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <vector>
@@ -21,6 +22,62 @@ struct Light {
     float far_z = 25.0f;
     unsigned int shadow_width = 1024;
     unsigned int shadow_height = 1024;
+
+    //attributes for shadowmaps 
+    //for spotlights and directional
+    GLuint shadow_fbo_2d   = 0;
+    GLuint shadow_map_2d   = 0;
+    //for point lights
+    GLuint shadow_fbo_cube = 0;
+    GLuint shadow_map_cube = 0;
+
+    Light(){
+
+        if (type == POINT){
+            glGenFramebuffers(1, &shadow_fbo_cube);
+            glGenTextures    (1, &shadow_map_cube);
+            glBindTexture(GL_TEXTURE_CUBE_MAP, shadow_map_cube);
+            for (int face = 0; face < 6; ++face) {
+                glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + face, 0,
+                            GL_DEPTH_COMPONENT,
+                            shadow_width, shadow_height, 0,
+                            GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+            }
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S,     GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T,     GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R,     GL_CLAMP_TO_EDGE);
+        }else{
+            glGenFramebuffers(1, &shadow_fbo_2d);
+            glGenTextures    (1, &shadow_map_2d);
+            glBindTexture(GL_TEXTURE_2D, shadow_map_2d);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+                        shadow_width, shadow_height, 0,
+                        GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+            //set texture attributes
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,     GL_CLAMP_TO_BORDER);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,     GL_CLAMP_TO_BORDER);
+            float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+            glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+            glBindFramebuffer(GL_FRAMEBUFFER, shadow_fbo_2d);
+            glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+                                    GL_TEXTURE_2D, shadow_map_2d, 0);
+            glDrawBuffer(GL_NONE); glReadBuffer(GL_NONE);
+        }
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    }
+
+    ~Light(){
+        if (shadow_fbo_2d)   glDeleteFramebuffers(1, &shadow_fbo_2d);
+        if (shadow_map_2d)   glDeleteTextures    (1, &shadow_map_2d);
+        if (shadow_fbo_cube) glDeleteFramebuffers(1, &shadow_map_cube);
+        if (shadow_map_cube) glDeleteTextures    (1, &shadow_map_cube);
+    }
 
     // Directional and spot lights use a single light-space matrix
     glm::mat4 get_light_view_matrix() const {
