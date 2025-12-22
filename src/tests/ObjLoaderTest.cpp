1da11afbe9f1edf19494cf76a35c6dea4a23350e
GeorgeFkd <geonfak76@gmail.com>
2025-12-22 20:20:48 +0200

    functioning test,just needs to solve the file problem for now, should run it from within the dir

---
+++ src/tests/ObjLoaderTest.cpp
@@ -0,0 +1,23 @@
+#include "../OBJLoader.h"
+#include <gtest/gtest.h>
+
+
+TEST(ObjLoader, BasicAssertions) {
+
+    ObjectLoader::OBJLoader loader;
+    auto filename = "./assets/test.obj";
+    loader.read_from_file(filename);
+    loader.debug_dump();
+
+    std::cout << "Finished loading.\n";
+    // std::cout << "Vertices:        " << loader.m_vertices.size() << "\n";
+    std::cout << "Vertices new:    " << loader.model_data.m_vertices.size() << "\n";
+    // std::cout << "Normals:         " << loader.m_vertex_normals.size() << "\n";
+    std::cout << "Normals new:     " << loader.model_data.m_vertex_normals.size() << "\n";
+    // std::cout << "TexCoords:       " << loader.m_texture_coords.size() << "\n";
+    std::cout << "TexCoords new:   " << loader.model_data.m_texture_coords.size() << "\n";
+    // std::cout << "Faces:           " << loader.m_faces.size() << "\n";
+    std::cout << "Faces new:       " << loader.model_data.m_faces.size() << "\n";
+
+}
+
