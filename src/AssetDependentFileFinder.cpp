3800d81a09a23a94befdb316d588420f4bcf9bfc
GeorgeFkd <geonfak76@gmail.com>
2026-03-25 18:26:03 +0200

    now can also get mentioned files in the assets

---
+++ src/AssetDependentFileFinder.cpp
@@ -0,0 +1,75 @@
+#include "loaders/ModelLoader.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+struct CliInput {
+    char* mentionedFilesInCodeFilename;
+    char* outputFilename;
+};
+
+void printExecInfo(CliInput* input) {
+    std::cout << "Input for dependent files finder is: " << input->mentionedFilesInCodeFilename
+              << "\n";
+}
+
+std::vector<std::string> extractDependentsFrom(const std::string& filename) {
+    auto ext   = std::filesystem::path(filename).extension();
+    std::cout << "Extension is: " << ext << "\n";
+    if (ext != ".obj") {
+        return {};
+    }
+    auto mdata = ModelLoader::load_or_get_cached(filename);
+    
+    std::cout << "Reading file " << filename << " to extract dependencies.\n";
+
+    std::vector<std::string> dependents;
+    for (const auto& submesh : mdata->submeshes) {
+        std::cout << "Material name: " << submesh.mat.name << "\n";
+        for (const auto& map :
+             {submesh.mat.map_Ka, submesh.mat.map_Kd, submesh.mat.map_Ks, submesh.mat.map_Bump}) {
+            if (!map.empty())
+                dependents.push_back(map);
+        }
+    }
+    return dependents;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <mentionedFiles.txt> <output.txt>\n";
+        return EXIT_FAILURE;
+    }
+    CliInput input = CliInput{.mentionedFilesInCodeFilename = argv[1], .outputFilename = argv[2]};
+    printExecInfo(&input);
+
+    std::ifstream file(input.mentionedFilesInCodeFilename);
+    if (!file) {
+        std::cerr << "Failed to open " << input.mentionedFilesInCodeFilename << "\n";
+        return EXIT_FAILURE;
+    }
+
+    std::vector<std::string> allFiles;
+    std::string              line;
+    while (std::getline(file, line)) {
+        if (line.empty())
+            continue;
+        allFiles.push_back(line);
+        std::cout << "Reading file " << line << " to extract dependencies.\n";
+
+        auto dependents = extractDependentsFrom(line);
+        allFiles.insert(allFiles.end(), dependents.begin(), dependents.end());
+    }
+
+    std::ofstream output(input.outputFilename);
+    if (!output) {
+        std::cerr << "Failed to open output file\n";
+        return EXIT_FAILURE;
+    }
+    for (const auto& f : allFiles) {
+        output << f << "\n";
+    }
+
+    return EXIT_SUCCESS;
+}
--- src/loaders/ModelLoader.h
+++ src/loaders/ModelLoader.h
@@ -3,7 +3,6 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
-#include "EntityID.h"
 #include <functional>
 #include <numeric>
 #include "scene_objects/MData.h"
